f5f017b1de66523ae8ba3ea49836d22f5d5e36b9
haobayuxi <whaohit@gmail.com>
2023-04-20 21:23:16 +0800

    update

--- core/common/msg_queue.cc
+++ core/common/msg_queue.cc
@@ -47,7 +47,7 @@ bool Msg_Queue::put(void* write) {
   memcpy(queue + tail_temp, write, 8);
   // printf("put queue success %d\n", ((struct SerializedReply*)queue)->size);
   struct SerializedReply* reply = (struct SerializedReply*)malloc(8);
-  get(reply);
+  get((void*)reply);
   printf("get reply%d\n", reply->size);
   return true;
 }
