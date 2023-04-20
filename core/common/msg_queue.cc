68c322dcaa9527d1b9888267dd4493412165ab96
haobayuxi <whaohit@gmail.com>
2023-04-20 21:02:31 +0800

    update

--- core/common/msg_queue.cc
+++ core/common/msg_queue.cc
@@ -22,7 +22,7 @@ bool Msg_Queue::is_empty() {
 bool Msg_Queue::get(void* read) {
   if (head != tail) {
     // get msg
-    memcpy(read, queue + head, 8);
+    memcpy(&read, queue + head, 8);
     head = (head + 1) % queue_size;
     return true;
   }
@@ -44,7 +44,7 @@ bool Msg_Queue::put(void* write) {
   } while (!tail.compare_exchange_strong(tail_temp, tail_temp + 1,
                                          memory_order_relaxed));
 
-  memcpy(queue + tail_temp, write, 8);
+  memcpy(queue + tail_temp, &write, 8);
   printf("put queue success %d\n", ((struct SerializedReply*)queue)->size);
   return true;
 }
