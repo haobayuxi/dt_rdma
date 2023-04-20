27a586ed816e3925dee4945d31f181b7ce674c84
haobayuxi <whaohit@gmail.com>
2023-04-20 09:48:40 +0800

    update

--- peer/server/server.cc
+++ peer/server/server.cc
@@ -63,12 +63,12 @@ int main(int argc, char *argv[]) {
   } else {
     printf("put fail");
   }
-  if (queue->get(&t2)) {
+  if (queue->get(t2)) {
     printf("get success\n");
   } else {
     printf("get fail");
   }
-  printf("t2.test = %d\n", t2.test);
+  printf("t2.test = %d\n", t2->test);
   // auto manager = new QP_Server_Manager(10001);
 
   // while (1) {
