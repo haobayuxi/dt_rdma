f7e8b361543b4b4a7a1e15924ea85763bcc27928
haobayuxi <whaohit@gmail.com>
2023-04-20 15:59:03 +0800

    update

--- peer/server/server.h
+++ peer/server/server.h
@@ -3,7 +3,7 @@
 
 #include "common/msg_queue.h"
 #include "connection/rdma.h"
-
+#include "worker/worker.h"
 // class Server {
 //  public:
 //   Server(int id, SystemConfig config) : server_id(id), config(config) {}
